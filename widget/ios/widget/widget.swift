d842952ca2904ec7f3c28cbdb3d0b3dea2dc1dfd
Igor Khramtsov <igor.khramtsov@proton.me>
2024-09-02 11:15:41 +0400

    feat(iOS): smart forecast widget

--- widget/ios/widget/widget.swift
+++ widget/ios/widget/widget.swift
@@ -7,6 +7,69 @@ import WidgetKit
 import SwiftUI
 import SQLite3
 
+extension Int {
+  var ordinalSuffix: String {
+    switch self {
+    case 1, 21, 31: return "st"
+    case 2, 22: return "nd"
+    case 3, 23: return "rd"
+    default: return "th"
+    }
+  }
+}
+
+extension Date {
+  func formattedWithOrdinal() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MMMM"
+    let month = dateFormatter.string(from: self)
+    let day = Calendar.current.component(.day, from: self)
+    return "\(month) \(day)\(day.ordinalSuffix)"
+  }
+  
+  func formattedLocalized() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.setLocalizedDateFormatFromTemplate("MMMMd")
+    return dateFormatter.string(from: self)
+  }
+  
+  func formattedDayOfWeek() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE"
+    return dateFormatter.string(from: self)
+  }
+
+  func formattedInHours() -> String {
+    let formatter = DateFormatter()
+    formatter.timeStyle = .short
+    return formatter.string(from: self)
+  }
+  
+  func formattedInHoursShort() -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: Locale.current)
+//    formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: Locale(identifier: "en_UK"))
+    return formatter.string(from: self)
+  }
+}
+
+extension Calendar {
+  func isDateInCurrentWeek(_ date: Date) -> Bool {
+    // Get the start of the current week
+    guard let startOfWeek = self.dateInterval(of: .weekOfYear, for: .now)?.start else {
+      return false
+    }
+      
+    // Get the end of the current week by adding 6 days to the start of the week
+    guard let endOfWeek = self.date(byAdding: .day, value: 6, to: startOfWeek) else {
+      return false
+    }
+    
+    // Check if the date is within the current week
+    return date >= startOfWeek && date <= endOfWeek
+  }
+}
+
 struct Provider: TimelineProvider {
   private let userDefaults: UserDefaults? = UserDefaults(suiteName: "group.dev.khramtsov.wanikani")
   private let db = DatabaseService()
@@ -31,21 +94,22 @@ struct Provider: TimelineProvider {
       .filter { entry in entry.0 <= currentDateInSeconds }
       .reduce(into: 0) { acc, entry in acc += entry.1 }
 
-    var forecast = reviewCounts.filter {entry in entry.0 > currentDateInSeconds }
-    forecast.insert((0, reviewsAvailableRightNow), at: 0)
+    let forecast = reviewCounts.filter {entry in entry.0 > currentDateInSeconds }
+    var forecast24hrs = forecast.filter {e in e.0 <= (forecast.first?.0 ?? 0) + 86400}
+    forecast24hrs.insert((0, reviewsAvailableRightNow), at: 0)
 
     for _ in 0 ..< 5 {
-      if (forecast.isEmpty) {
+      if (forecast24hrs.isEmpty) {
         break
       }
-      let forecastEntry = forecast.removeFirst()
+      let forecastEntry = forecast24hrs.removeFirst()
 
       let entry = SimpleEntry(
         date: forecastEntry.0 == 0 ? .now : Date(timeIntervalSince1970: TimeInterval(forecastEntry.0)),
         availableReviews: (entries.last?.availableReviews ?? 0) + forecastEntry.1,
         forecast: context.family == .systemSmall
         ? nil
-        : forecast.prefix(5).map { e in
+        : forecast24hrs.prefix(5).map { e in
           (Date(timeIntervalSince1970: TimeInterval(e.0)), e.1)
         }
       )
@@ -69,12 +133,6 @@ struct widgetEntryView: View {
   var body: some View {
     let forecast = entry.forecast
     
-    func formatDate(_ date: Date) -> String {
-      let formatter = DateFormatter()
-      formatter.dateFormat = "h a"
-      return formatter.string(from: date)
-    }
-    
     return HStack() {
       VStack {
         Text(entry.availableReviews.formatted())
@@ -88,14 +146,26 @@ struct widgetEntryView: View {
           .aspectRatio(contentMode: .fit)
       }
       
-      if let forecast = forecast {
+      if let forecast = forecast, !forecast.isEmpty {
+        let firstDate = forecast.first!.0
         let maxForecastReviews = forecast.max { e1, e2 in e1.1 < e2.1}?.1 ?? 1
           VStack {
-            Text("Today's Forecast")
-              .font(.headline)
-              .foregroundStyle(.white)
-              Spacer()
+            Text(
+              Calendar.current.isDateInToday(firstDate)
+              ? "Today's Forecast"
+              : Calendar.current.isDateInTomorrow(firstDate)
+              ? "Tomorrow's Forecast"
+              : Calendar.current.isDateInCurrentWeek(firstDate)
+              ? "\(firstDate.formattedDayOfWeek())'s Forecast"
+              : firstDate.formattedLocalized()
+            )
+            .font(.headline)
+            .foregroundStyle(.white)
+            
+            Spacer()
+            
             HStack(alignment: .bottom) {
+              Spacer()
               ForEach(forecast.prefix(5), id: \.0) { date, reviewsCount in
                 let height = 50.0 * (Double(reviewsCount) / Double(maxForecastReviews))
                 VStack {
@@ -105,10 +175,11 @@ struct widgetEntryView: View {
                   RoundedRectangle(cornerRadius: 4)
                     .frame(width: 10, height: height)
                     .foregroundStyle(Color("Green"))
-                  Text(formatDate(date))
+                  Text(date.formattedInHoursShort())
                     .font(.caption)
                     .foregroundStyle(.white.opacity(0.75))
                 }
+                Spacer()
               }
             }.frame(maxWidth: .infinity, alignment: .leading)
           }
@@ -158,7 +229,7 @@ struct widget: Widget {
 // }
 
 @available(iOS 17.0, *)
-#Preview(as: .systemMedium) {
+#Preview("Today", as: .systemMedium) {
   widget()
 } timeline: {
   SimpleEntry(
@@ -173,7 +244,52 @@ struct widget: Widget {
   ])
 }
 @available(iOS 17.0, *)
-#Preview(as: .systemSmall) {
+#Preview("Tomorrow", as: .systemMedium) {
+  widget()
+} timeline: {
+  SimpleEntry(
+    date: .now,
+    availableReviews: 239,
+    forecast: [
+      (.now.addingTimeInterval(25 * 60 * 60), 5),
+      (.now.addingTimeInterval(27 * 60 * 60), 10),
+      (.now.addingTimeInterval(28 * 60 * 60), 5),
+      (.now.addingTimeInterval(29 * 60 * 60), 8),
+      (.now.addingTimeInterval(30 * 60 * 60), 8),
+  ])
+}
+@available(iOS 17.0, *)
+#Preview("This week", as: .systemMedium) {
+  widget()
+} timeline: {
+  SimpleEntry(
+    date: .now,
+    availableReviews: 239,
+    forecast: [
+      (.now.addingTimeInterval(85 * 60 * 60), 5),
+      (.now.addingTimeInterval(87 * 60 * 60), 10),
+      (.now.addingTimeInterval(88 * 60 * 60), 5),
+      (.now.addingTimeInterval(89 * 60 * 60), 8),
+      (.now.addingTimeInterval(80 * 60 * 60), 8),
+  ])
+}
+@available(iOS 17.0, *)
+#Preview("Next week", as: .systemMedium) {
+  widget()
+} timeline: {
+  SimpleEntry(
+    date: .now,
+    availableReviews: 239,
+    forecast: [
+      (.now.addingTimeInterval(185 * 60 * 60), 5),
+      (.now.addingTimeInterval(187 * 60 * 60), 10),
+      (.now.addingTimeInterval(188 * 60 * 60), 5),
+      (.now.addingTimeInterval(189 * 60 * 60), 8),
+      (.now.addingTimeInterval(180 * 60 * 60), 8),
+  ])
+}
+@available(iOS 17.0, *)
+#Preview("Small", as: .systemSmall) {
   widget()
 } timeline: {
   SimpleEntry(
