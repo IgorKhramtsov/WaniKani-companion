e9cb8f09b53da7f3d1049673d714a8170c2d012e
Igor Khramtsov <igor.khramtsov@proton.me>
2024-09-05 12:08:20 +0400

    refactor: use new version of expo-sqlite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,16 +6,12 @@ import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { RootSiblingParent } from 'react-native-root-siblings'
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useReactQueryDevTools } from '@dev-plugins/react-query/build/useReactQueryDevTools'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useMemo } from 'react'
 import { Platform, Text } from 'react-native'
 import * as FS from 'expo-file-system'
-import { FullPageLoading } from '@/src/components/FullPageLoading'
+import { Directory } from 'expo-file-system/next'
 
 const queryClient = new QueryClient()
 
@@ -24,54 +20,34 @@ export default function RootLayout() {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useReactQueryDevTools(queryClient)
   }
+  const dbDirectory = useMemo(() => {
+    if (Platform.OS === 'ios') {
+      return Directory.getSharedContainerUri('group.dev.khramtsov.wanikani')
+        ?.path
+    }
+    return FS.documentDirectory + 'SQLite'
+  }, [])
+  if (dbDirectory === null) {
+    return <Text>Cannot access db directory (it's null)</Text>
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SessionProvider>
         <QueryClientProvider client={queryClient}>
-          <SqliteProvider>
+          <SQLiteProvider
+            databaseName='wanikani.db'
+            directory={dbDirectory}
+            onInit={db => dbHelper.createTables(db)}>
             <StoreProvider>
               <RootSiblingParent>
                 <Slot />
               </RootSiblingParent>
             </StoreProvider>
-          </SqliteProvider>
+          </SQLiteProvider>
         </QueryClientProvider>
       </SessionProvider>
     </GestureHandlerRootView>
-  )
-}
-
-/// A separate component to be able to access sqlite context
-const SqliteProvider = ({ children }: PropsWithChildren) => {
-  const dbDirectory = useQuery({
-    queryKey: ['dbDirectory'],
-    queryFn: async () => {
-      if (Platform.OS === 'ios') {
-        return await FS.getSharedContainerUriAsync(
-          'group.dev.khramtsov.wanikani',
-        )
-      }
-      return FS.documentDirectory + 'SQLite'
-    },
-  })
-  if (dbDirectory.isPending) {
-    return <FullPageLoading />
-  }
-  if (dbDirectory.isError) {
-    return <Text>Cannot access db directory {dbDirectory.error.message}</Text>
-  }
-  if (dbDirectory.data === null) {
-    return <Text>Cannot access db directory (it's null)</Text>
-  }
-
-  return (
-    <SQLiteProvider
-      databaseName='wanikani.db'
-      directory={dbDirectory.data}
-      onInit={db => dbHelper.createTables(db)}>
-      {children}
-    </SQLiteProvider>
   )
 }
 
