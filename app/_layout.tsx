7ce726517bd6704cfe78cc6b989a2545560a57ae
Igor Khramtsov <igor.khramtsov@proton.me>
2024-10-10 09:21:45 +0400

    feat: add sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,14 @@
 import { SessionProvider } from '@/src/context/authContext'
 import { createStore } from '@/src/redux/store'
 import { dbHelper } from '@/src/utils/dbHelper'
-import { Slot } from 'expo-router'
+import { Slot, useNavigationContainerRef } from 'expo-router'
 import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { Provider } from 'react-redux'
 import { RootSiblingParent } from 'react-native-root-siblings'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useReactQueryDevTools } from '@dev-plugins/react-query/build/useReactQueryDevTools'
-import { PropsWithChildren, useMemo } from 'react'
+import { PropsWithChildren, useEffect, useMemo } from 'react'
 import { Platform, Text, useColorScheme } from 'react-native'
 import * as FS from 'expo-file-system'
 import { Directory } from 'expo-file-system/next'
@@ -17,14 +17,41 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native'
+import * as Sentry from '@sentry/react-native'
+import { isRunningInExpoGo } from 'expo'
 
 const queryClient = new QueryClient()
 
-export default function RootLayout() {
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation()
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  debug: true,
+  tracesSampleRate: 0.2,
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // Pass instrumentation to be used as `routingInstrumentation`
+      routingInstrumentation,
+      enableNativeFramesTracking: !isRunningInExpoGo(),
+      // ...
+    }),
+  ],
+})
+
+export function RootLayout() {
   if (__DEV__) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useReactQueryDevTools(queryClient)
   }
+  // Capture the NavigationContainer ref and register it with the instrumentation.
+  const ref = useNavigationContainerRef()
+
+  useEffect(() => {
+    if (ref) {
+      routingInstrumentation.registerNavigationContainer(ref)
+    }
+  }, [ref])
   const colorScheme = useColorScheme()
   const dbDirectory = useMemo(() => {
     if (Platform.OS === 'ios') {
@@ -59,6 +86,8 @@ export default function RootLayout() {
     </GestureHandlerRootView>
   )
 }
+
+export default Sentry.wrap(RootLayout)
 
 /// A separate component to be able to access sqlite context
 const StoreProvider = ({ children }: PropsWithChildren) => {
